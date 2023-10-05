ad38777467f945dbe82ad59163a5aed4a97f455e
Safin Rashid <srashid@utexas.edu>
2023-10-05 13:57:20 -0500

    Login/Signup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoginForm from './components/loginform';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
