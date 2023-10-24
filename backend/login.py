ad38104b2b270449a36e3839e844cca7dec7476c
Exium <exium16@gmail.com>
2023-10-24 13:47:33 -0500

    front to back end interaction for logging in with sessions

--- backend/login.py
+++ backend/login.py
@@ -4,14 +4,16 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 from pymongo import MongoClient
+import uuid
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 # MongoDB connection setup
-client = MongoClient("mongodb+srv://valinsky:lF4rTL8YTLpYXhXF@cluster0.xfvk0tz.mongodb.net/")
+client = MongoClient("mongodb+srv://admin:securepassword@cluster0.i1vtszj.mongodb.net/?retryWrites=true&w=majority")
 db = client["UserInfo"]
 users_collection = db["Users"]
+projects_collection = db["Projects"]
 # client = MongoClient("mongodb+srv://srashid:4j7MYn0GiqTjY3R1@project.mvmtzkd.mongodb.net/")
 # db = client.Users
 # users_collection = db.UserInfo
@@ -36,6 +38,7 @@ def register_user():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
+    userID = uuid.uuid4().hex
 
     # Encrypt the password before storing it
     # encrypted_password = encrypt(password.encode('utf-8'), encryption_key)
@@ -47,13 +50,14 @@ def register_user():
     user_data = {
         "username": username,
         #encrypted_password.hex()
-        "password": password  # Store the hex representation of the encrypted password
+        "password": password,  # Store the hex representation of the encrypted password
+        "userID": userID # used on client side for session management with cookies
     }
 
     # Insert the user data into MongoDB
     users_collection.insert_one(user_data)
 
-    return jsonify({"message": "User registered successfully"}), 201
+    return jsonify({"message": "User registered successfully", "userID": userID}), 201
 
 @app.route('/login', methods=['POST'])
 def login_user():
@@ -65,15 +69,75 @@ def login_user():
 
     if user:
         #stored_password = bytes.fromhex(user['password'])
-        decrypted_password = password
+        # decrypted_password = password
         #decrypted_password = decrypt(stored_password, encryption_key).decode('utf-8')
         
-        if password == decrypted_password:
-            return jsonify({"message": "Login successful"}), 200
+        if password == user['password']:
+            return jsonify({"message": "Login successful", "userID": user["userID"]}), 200
         else:
             return jsonify({"message": "Invalid password"}), 401
 
     return jsonify({"message": "User not found"}), 404
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    headers = request.headers
+    bearer = headers.get('Authorization')    # Bearer YourTokenHere
+    token = bearer.split()[1]  # YourTokenHere
+
+    if (token == None):
+        return jsonify({"message": "No auth"}), 400
+
+    # Find the project that the user is a part of
+    projects = projects_collection.find({"users": {"$in": [token]}})
+
+    project_list = []
+    for project in projects:
+        project_list.append({
+            "id": project["id"],
+            "name": project["name"],
+            "desc": project["desc"],
+        })
+
+    print(project_list)
+
+    if project_list:
+        return jsonify({"message": "Projects found", "projects": project_list}), 200
+    else:
+        return jsonify({"message": "Projects not found"}), 400
+    
+@app.route('/projects', methods=['POST'])
+def new_project():
+    headers = request.headers
+    bearer = headers.get('Authorization')    # Bearer YourTokenHere
+    token = bearer.split()[1]  # YourTokenHere
+    
+    if (token == None):
+        return jsonify({"message": "No auth"}), 400
+    
+    data = request.get_json()
+
+    projectID = data.get("id")
+    name = data.get("name")
+    desc = data.get("desc")
+
+    project_data = {
+        "id": projectID,
+        "name": name,
+        "desc": desc,
+        "users": [token],
+        "hwSets": []
+    }
+
+    projects_collection.insert_one(project_data)
+
+    return jsonify({"message": "Project created successfully", "project": {
+        "id": projectID,
+        "name": name,
+        "desc": desc,
+        "users": [token],
+        "hwSets": []
+    }}), 201
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,19 +6,22 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import NewProject from './pages/NewProject';
 import ProjectPage from './pages/ProjectPage';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/projects" element={<Home/>}/>
-      <Route path="/projects/:id" element={<ProjectPage/>}/>
-      <Route path="/projects/new" element={<NewProject/>}/>
-    </Routes>
-  </BrowserRouter>
+  <CookiesProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/projects" element={<Home/>}/>
+        <Route path="/projects/:id" element={<ProjectPage/>}/>
+        <Route path="/projects/new" element={<NewProject/>}/>
+      </Routes>
+    </BrowserRouter>
+  </CookiesProvider>
 );
 
 reportWebVitals();
